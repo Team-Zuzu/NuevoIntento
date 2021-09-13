7b0a85c42ba82a4971fa272d5516635723e8ba8c
MatiasEsp404 <unity.matias@outlook.com>
2021-09-13 14:07:32 -0300

    Water terminado

--- Matias/c-otros-proyectos/b-build-a-responsive-glassmorphism-website-with-animation-from-scratch-using-html-css-and-js/js.js
+++ Matias/c-otros-proyectos/b-build-a-responsive-glassmorphism-website-with-animation-from-scratch-using-html-css-and-js/js.js
@@ -1,4 +1,6 @@
 function menuToggle(){
     const toggleMenu = document.querySelector('.toggle');
+    const section = document.querySelector('section');
     toggleMenu.classList.toggle('active');
+    section.classList.toggle('active');
 }
