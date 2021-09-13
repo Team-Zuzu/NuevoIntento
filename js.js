b6d4e079a6a87579ea24fc64b328635e8c7e7db5
MatiasEsp404 <unity.matias@outlook.com>
2021-09-13 18:17:33 -0300

    soymatias en Js

--- js.js
+++ js.js
@@ -1,17 +1,32 @@
-const puntos = 0;
+let pos = 0;
+const truco = ("soymatias");
+const trucoPorLetra = Array.from(truco);
 
-document.addEventListener("keydown", function (event) {
-	if (event.key === "ArrowUp") {
-		console.log(`arriba`);
-        puntos++;
-        console.log(puntos);
-	}
-	else if (event.key === "ArrowDown") {
-		console.log(`abajo`);
-        puntos++;
-        console.log(puntos);
-	}
-    else{
-        
+document.addEventListener("keydown", function (letra) {
+
+    if (letra.key === trucoPorLetra[pos]) {
+        console.log(letra.key);
+        pos++;
+    }
+    else {
+        console.log(`${letra.key} no es igual a ${trucoPorLetra.indexOf}. Posición reiniciada.`);
+        pos = 0;
     }
 });
+
+const update = setInterval(() => {
+    if (pos == trucoPorLetra.length) {
+        clearInterval(update);
+        console.log('Hola Matías');
+
+        const lista = document.getElementById('lista');
+
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = './Matias/c-otros-proyectos/otros-proyectos.html'
+        a.textContent = 'Otros Proyectos';
+
+        li.appendChild(a);
+        lista.insertAdjacentElement('beforeend', li);
+    }
+}, 200);
