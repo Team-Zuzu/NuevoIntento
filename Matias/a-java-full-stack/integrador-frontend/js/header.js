3aad7b01234bc42ea5751d71ca7cf22f95a97e0d
MatiasEsp404 <unity.matias@outlook.com>
2021-09-24 01:50:38 -0300

    Tp integrador

--- Matias/a-java-full-stack/integrador-frontend/js/header.js
+++ Matias/a-java-full-stack/integrador-frontend/js/header.js
@@ -1,4 +1,25 @@
+let header = document.querySelector('header');
+let hamburguesa = document.querySelector('.hamburguesa');
+
 window.addEventListener('scroll', () => {
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
+
+    if (window.innerWidth > 1200) {
+        header.classList.toggle('sticky', window.scrollY > 0);
+    }
 })
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1200) {
+        if (window.scrollY > 0 && !header.classList.contains('sticky')) {
+            header.classList.add('sticky');
+        }
+    }
+    else {
+        header.classList.remove('sticky', header.classList.contains('sticky'));
+    }
+})
+
+const MenuHamburguesa = () => {
+    header.classList.toggle('active');
+    hamburguesa.classList.toggle('active');
+}
