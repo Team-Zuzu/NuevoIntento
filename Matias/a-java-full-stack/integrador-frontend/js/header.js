b3f24d39ebbdab75c08ea77d1d5e293c00716589
MatiasEsp404 <unity.matias@outlook.com>
2021-09-26 21:30:26 -0300

    Carrusel
    Carrusel terminado. Falta dejarlo lindo

--- Matias/a-java-full-stack/integrador-frontend/js/header.js
+++ Matias/a-java-full-stack/integrador-frontend/js/header.js
@@ -1,5 +1,7 @@
 let navbar = document.querySelector('.navbar');
 let toggle = document.querySelector('.toggle');
+let imgs = document.querySelectorAll('.img-speaker');
+let descriptions = document.querySelectorAll('.description');
 
 window.addEventListener('scroll', () => {
 
@@ -31,3 +33,60 @@ VanillaTilt.init(document.querySelectorAll('.card'), {
     max: 15,
     speed: 1000,
 });
+
+const SearchChecked = () => {
+    let found = false
+    let i = 0
+    while (!found && i < imgs.length) {
+        if (imgs[i].classList.contains('checked')) {
+            found = true
+        }
+        else {
+            i++
+        }
+    }
+    if (found) {
+        return i
+    }
+    else {
+        console.error('Checked not found')
+        // voy a adoptar el -1 como "código de error" para evitar que los siguientes métodos intenten acceder al array
+        return -1
+    }
+}
+
+const NextImg = () => {
+    let i = SearchChecked()
+    if (i >= 0) {
+        imgs[i].classList.remove('checked')
+        descriptions[i].classList.remove('checked')
+        if (i == imgs.length - 1) {
+            //vuelve a la primera carta
+            i = 0
+        }
+        else {
+            //pasa a la siguiente carta
+            i++
+        }
+        imgs[i].classList.add('checked')
+        descriptions[i].classList.add('checked')
+    }
+}
+
+const PreviousImg = () => {
+    let i = SearchChecked()
+    if (i >= 0) {
+        imgs[i].classList.remove('checked')
+        descriptions[i].classList.remove('checked')
+        if (i == 0) {
+            //pasa a la ultima carta
+            i = imgs.length - 1
+        }
+        else {
+            //vuelve a la carta anterior
+            i--
+        }
+        imgs[i].classList.add('checked')
+        descriptions[i].classList.add('checked')
+    }
+}
