2ef8f3b3772a9e5b377fe9f59a009b7604fc3954
MatiasEsp404 <unity.matias@outlook.com>
2021-10-09 22:59:00 -0300

    Animación terminada

--- Matias/a-java-full-stack/trabajos-practicos/integrador-frontend/js/load.js
+++ Matias/a-java-full-stack/trabajos-practicos/integrador-frontend/js/load.js
@@ -2,14 +2,7 @@ const loader = document.querySelector('.loader')
 var dosSegundos = false
 var cargaTerminada = false
 
-function agregarClases(cad) {
-    for (let i = 0; i < loader.childElementCount; i++) {
-        loader.children[i].classList.add(cad)
-    }
-}
-
 function fin() {
-    // alert('fin')
     loader.classList.add('fin')
     document.querySelector('header').classList.add('fin')
     document.querySelector('main').classList.add('fin')
@@ -17,37 +10,23 @@ function fin() {
     document.querySelector('body').classList.add('fin')
 }
 
-// if (window.scrollY == 0) {
-//     window.addEventListener('click', event => {
-//         cargaTerminada = true
-//     });
-// }
-// else {
-//     fin()
-//     console.log(window.scrollY)
-// }
-window.addEventListener('click', event => {
+window.addEventListener('load', event => {
     cargaTerminada = true
 });
 
 setTimeout(() => {
     dosSegundos = true
-}, 2000)
+}, 2500)
 
-// const disparador = setInterval(() => {
-//     if (dosSegundos && cargaTerminada) {
-//         clearInterval(disparador)
-//         agregarClases('carga-terminada');
-//         setTimeout(() => {
-//             agregarClases('codo-a-codo');
-//             loader.classList.add('codo-a-codo')
-//         }, 1250);
-//         setTimeout(() => {
-//             fin()
-//         }, 3000)
-//     }
-// }, 200);
-
-agregarClases('carga-terminada');
-agregarClases('codo-a-codo');
-loader.classList.add('codo-a-codo')
+const disparador = setInterval(() => {
+    if (dosSegundos && cargaTerminada) {
+        clearInterval(disparador)
+        loader.classList.add('carga-terminada')
+        setTimeout(() => {
+            loader.classList.add('codo-a-codo')
+        }, 1250);
+        setTimeout(() => {
+            fin()
+        }, 3000)
+    }
+}, 200);
