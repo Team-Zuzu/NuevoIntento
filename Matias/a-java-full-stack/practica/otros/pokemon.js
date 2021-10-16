1167bc1086183121b0267698f16d43bdf2773ac6
MatiasEsp404 <unity.matias@outlook.com>
2021-10-15 23:13:40 -0300

    Pokemon

--- Matias/a-java-full-stack/practica/otros/pokemon.js
+++ Matias/a-java-full-stack/practica/otros/pokemon.js
@@ -1,23 +1,47 @@
-// new Vue({
-//     el: "#pokemon",
-//     data: {
-//         info: '',
-//     },
-//     created() {
-//         this.cargaDatos("https://pokeapi.co/api/v2/pokemon")
-//     },
-//     methods: {
-//         cargaDatos(url) {
-//             fetch(url)
-//                 .then(response => response.json())
-//                 .then(data => {
-//                     console.log('Llegaron los datos');
-//                     this.info = data
-//                 })
-//                 .catch(error => {
-//                     console.log('Hay un error raro e.e');
-//                     console.log(error)
-//                 })
-//         }
-//     }
-// })
+botones = {
+    siguiente: null,
+    anterior: null
+}
+
+function botonSiguiente() {
+    botones.siguiente != null ? cargaDatos(botones.siguiente) : console.log('Llegaste al final, sorry')
+}
+
+function botonAnterior() {
+    botones.anterior != null ? cargaDatos(botones.anterior) : console.log('No podés ir más atrás')
+}
+
+window.addEventListener('load', load => {
+    cargaDatos("https://pokeapi.co/api/v2/pokemon")
+})
+
+function cargaDatos(url) {
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(data => {
+            const cuerpo = document.querySelector('#cuerpo')
+            let nuevoCuerpo = document.createElement('section')
+            nuevoCuerpo.id = 'cuerpo'
+            const fragmento = document.createDocumentFragment()
+            for (let i = 0; i < data.results.length; i++) {
+                let nuevaTarjeta = document.createElement('div')
+                nuevaTarjeta.classList.add('tarjeta')
+                let nuevaImagen = document.createElement('img')
+                fetch(data.results[i].url)
+                    .then(respuesta => respuesta.json())
+                    .then(data2 => {
+                        nuevaImagen.src = data2.sprites.other.dream_world.front_default
+                    })
+                nuevaImagen.alt = data.results[i].name
+                nuevaTarjeta.insertAdjacentElement('afterbegin', nuevaImagen)
+                let nuevoParrafo = document.createElement('p')
+                nuevoParrafo.textContent = data.results[i].name
+                nuevaTarjeta.insertAdjacentElement('beforeend', nuevoParrafo)
+                fragmento.append(nuevaTarjeta)
+            }
+            nuevoCuerpo.append(fragmento)
+            cuerpo.replaceWith(nuevoCuerpo)
+            botones.siguiente = data.next
+            botones.anterior = data.previous
+        })
+}
